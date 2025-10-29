ec875d7f0776ec64e300f65407afe9003d3af1e2
Raj-GitCode <raaj21233@gmail.com>
2025-10-29 14:18:28 +0530

    Create 14_adc_defines.h

---
+++ src/14_adc_defines.h
@@ -0,0 +1,22 @@
+//adc_defines.h
+#ifndef ADC_DEFINES_H
+#define ADC_DEFINES_H
+
+// System clocks
+#define FOSC 12000000
+#define CCLK (FOSC * 5)       // 60 MHz
+#define PCLK (CCLK / 4)       // 15 MHz
+#define ADCCLK 3000000
+#define CLKDIV ((PCLK / ADCCLK) - 1)
+
+// ADC register bit defines
+#define CLKDIV_BITS    8
+#define PDN_BIT        21
+#define START_BIT      24
+#define RESULT_BITS     6
+#define DONE_BIT       31
+
+// ADC channel mapping
+#define ADC0_PINSEL   (1 << 22)   // P0.27 -> AD0.0
+
+#endif
