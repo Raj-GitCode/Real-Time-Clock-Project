f6cb73a10a66de3cc5041c1086b7fc89ad266c1f
Raj-GitCode <raaj21233@gmail.com>
2025-11-01 09:29:21 +0530

    Update 00_main_helper.c

--- RTC/00_main_helper.c
+++ RTC/00_main_helper.c
@@ -8,7 +8,7 @@
 #include "kpm.h"
 #include "adc.h"
 #include "dt.h"
-
+#include "bell.h"
 
 void Set_DateAlarm(void);
 void Set_Alarm(void);
@@ -23,20 +23,39 @@ extern void EINT0_ISR(void);
 
 void Check_Alarm(void)
 {
-    u32 i;
     static unsigned char alarm_triggered = 0;
+    unsigned char key;
 
-    if (alarm_hour == -1) return;  // No alarm set
+    if (alarm_hour == -1) 
+        return;  // No alarm set
 
+    // When current time matches the alarm
     if (hour == alarm_hour && min == alarm_min && sec == alarm_sec && alarm_triggered == 0)
     {
         alarm_triggered = 1;
-        IOSET0 = (1 << 0);    // Turn ON LED
-        for (i = 0; i < 5; i++)
-            delay_s(1);
-        IOCLR0 = (1 << 0);    // Turn OFF LED
+
+        CmdLCD(CLEAR_LCD);
+        Show_Bell_On_LCD();
+
+        IOSET0 = (1 << 0);   // Turn ON LED or Alarm output
+
+        // Keep alarm active until any key is pressed
+        while (1)
+        {
+            key = KeyScan();
+            if (key != '\0')   // Any key pressed
+            {
+                IOCLR0 = (1 << 0);     // Turn OFF LED
+                CmdLCD(CLEAR_LCD);     // Clear LCD
+                StrLCD("Alarm Stopped");
+                delay_ms(500);
+                CmdLCD(CLEAR_LCD);
+                break;
+            }
+        }
     }
 
+    // Reset trigger when time moves forward
     if (sec != alarm_sec)
         alarm_triggered = 0;
 }
@@ -57,20 +76,22 @@ void EINT0_Init(void)
     VICVectCntl0 = 0x20 | 14;
     VICIntEnable = (1 << 14);
 }
-
 void Show_RTC_Display(void)
 {
-    static int last_min = -1;   // Remember the last displayed minute
+    static int last_refresh_sec = -1;   // Store last refresh time
+    static unsigned char first_run = 1; // Track first execution
 
     GetRTCTimeInfo(&hour, &min, &sec);
     GetRTCDateInfo(&date, &month, &year);
     GetRTCDay(&day);
 
-    // Clear LCD only when minute changes (once per minute)
-    if (min != last_min)
+    // ? Clear LCD on first run, or every 15 seconds
+    if (first_run || last_refresh_sec == -1 ||
+        (sec - last_refresh_sec >= 15) || (sec < last_refresh_sec))
     {
         CmdLCD(CLEAR_LCD);
-        last_min = min;
+        last_refresh_sec = sec;
+        first_run = 0;
     }
 
     DisplayRTCTime(hour, min, sec);
@@ -80,6 +101,37 @@ void Show_RTC_Display(void)
     delay_ms(2);
 }
 
+/*
+void Show_RTC_Display(void)
+{
+    static int last_refresh_sec = -1;   // store last refresh time
+	
+    GetRTCTimeInfo(&hour, &min, &sec);
+    GetRTCDateInfo(&date, &month, &year);
+    GetRTCDay(&day);
+
+    // Clear LCD only when minute changes (once per minute)
+    ///if (min != last_min)
+    {
+        CmdLCD(CLEAR_LCD);
+        last_min = min;
+    }//
+			
+		
+    // Refresh every 15 seconds
+    if (last_refresh_sec == -1 || (sec - last_refresh_sec >= 15) || (sec < last_refresh_sec))
+    {
+        CmdLCD(CLEAR_LCD);
+        last_refresh_sec = sec;
+    }
+	
+    DisplayRTCTime(hour, min, sec);
+    DisplayRTCDay(day);
+    DisplayRTCDate(date, month, year);
+    CmdLCD(0x0C);
+    delay_ms(2);
+}
+*/
 void Show_ADC_Temperature(void)
 {
     unsigned int adcValue;
@@ -101,6 +153,31 @@ void Show_ADC_Temperature(void)
     StrLCD("C");
 }
 
+void Show_KeyHelp(void)
+{
+    CmdLCD(CLEAR_LCD);
+		StrLCD("  ***PRESS***");
+		delay_ms(100);
+		CmdLCD(CLEAR_LCD);
+    StrLCD((s8 *)" =Bck D:Fw B:Up");
+    CmdLCD(GOTO_LINE2_POS0+3);
+    StrLCD((s8 *)"C:Dn *:Menu");
+    delay_s(1);   // Show for 5 seconds
+    CmdLCD(CLEAR_LCD);
+}
+void Show_A_KeyHelp(void)
+{
+    CmdLCD(CLEAR_LCD);
+		StrLCD("  ***PRESS***");
+		delay_ms(100);
+		CmdLCD(CLEAR_LCD);
+    StrLCD((s8 *)"   =Bck D:Fw");
+    CmdLCD(GOTO_LINE2_POS0+4);
+    StrLCD((s8 *)"*:Menu");
+    delay_s(1);   // Show for 5 seconds
+    CmdLCD(CLEAR_LCD);
+}
+
 void Show_Edit_Menu(void)
 {
     CmdLCD(CLEAR_LCD);
@@ -115,6 +192,7 @@ void Show_Edit_Menu(void)
         switch (key)
         {
             case '1':
+								Show_KeyHelp();
                 Set_DateAlarm();
                 CmdLCD(CLEAR_LCD);
                 StrLCD("  ****EDIT****");
@@ -123,6 +201,8 @@ void Show_Edit_Menu(void)
                 break;
 
             case '2':
+								//Show_KeyHelp();
+								Show_A_KeyHelp();
                 Set_Alarm();
                 CmdLCD(CLEAR_LCD);
                 StrLCD("  ****EDIT****");
